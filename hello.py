cba4fa54d3859f5d8f293e02f6d3b1d3c13175ad
Dmitriy Rypalov <Dmitriy.Rypalov@ico.kz>
2016-11-30 14:10:01 +0600

    делете + селект

--- hello.py
+++ hello.py
@@ -1,5 +1,44 @@
 import requests
 __author__ = 'Dmitriy.Rypalov'
-print('Hello world')
 
-print(requests.get('http://fhirtest.uhn.ca/baseDstu3','Organization?_format=json&_pretty=true').json())
+
+import requests
+import json
+
+FHIR_URL = 'http://fhirtest.uhn.ca/baseDstu2/Patient'
+
+
+def list():
+    r = requests.get(FHIR_URL, dict(_format='json', _pretty=True))
+    return r.json()
+
+
+def create():
+    data = {
+        "resourceType": "Patient",
+        "name": [
+            {
+                "family": [
+                    "Fletchers"
+                ],
+                "given": [
+                    "Arturo",
+                    "F"
+                ]
+            }
+        ],
+        "gender": "unknown",
+        "birthDate": "1942-01-28",
+        "deceasedBoolean": False
+    }
+
+    r = requests.post(FHIR_URL, json.dumps(data))
+    r.headers = {'Accept': 'application/json+fhir', "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
+    return r.json()
+
+print(create())
+
+print(requests.get(FHIR_URL+'/128808').json())
+r = requests.delete(FHIR_URL+'/128808')
+
+#print(list()
