8b111a97d1949058443c1fc4e11f659187502a85
Dmitriy Rypalov <Dmitriy.Rypalov@ico.kz>
2016-11-30 13:03:03 +0600

    Каркас по БРАЦКИЙ

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
+import requests
 __author__ = 'Dmitriy.Rypalov'
-print ('Hello world')
+print('Hello world')
+
+print(requests.get('http://fhirtest.uhn.ca/baseDstu3','Organization?_format=json&_pretty=true').json())
